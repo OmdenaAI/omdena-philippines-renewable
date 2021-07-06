96f13d9aa1dbf1b87a7b392b1239ac9ada828cd0
eRuaro <erickson_ruaroii@dlsu.edu.ph>
2021-07-06 17:29:15 +0800

    Added sample pages section

--- src/visualizations/data-journal/main.py
+++ src/visualizations/data-journal/main.py
@@ -1,17 +1,28 @@
 import streamlit as st
+import pages.page1 as p1
+import pages.page2 as p2
 
-class HomePage():
+class HomePage:
     """
     This class serves as the main homepage for the Data Journal project.
     """
+    
     def __init__(self):
         self.title = "Data Journal"
         self.subtitle = "A Data-Driven Approach to the Visualization of the World"
         self.author = "Neil Ruaro"
+        self.PAGES = {
+            "Page 1" : p1.Page1,
+            "Page 2" : p2.Page2,
+        }
 
     def home_page(self):
         st.title(self.title)
         st.write(self.subtitle)
+        st.sidebar.title('Pages')
+        selection = st.sidebar.radio("Go to", list(self.PAGES.keys()))
+        page = self.PAGES[selection]
+        page.app()
 
 class Main():
     def __init__(self):
