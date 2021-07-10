5446d2d971c57031bc2d6c47d3524f2d74cd5857
eRuaro <erickson_ruaroii@dlsu.edu.ph>
2021-07-10 16:04:13 +0800

    Finished prototype for one task

--- src/visualizations/data-journal/main.py
+++ src/visualizations/data-journal/main.py
@@ -8,18 +8,14 @@ class HomePage:
     """
     
     def __init__(self):
-        self.title = "Data Journal"
-        self.subtitle = "Documenting everything from Data Processing to Data Modeling"
         self.author = "Neil Ruaro"
         self.PAGES = {
-            "Page 1" : p1.Page1,
+            "Estimating Energy Availability" : p1.Page1,
             "Page 2" : p2.Page2,
         }
 
     def home_page(self):
-        st.title(self.title)
-        st.write(self.subtitle)
-        st.sidebar.title('Pages')
+        st.sidebar.title('Tasks')
         selection = st.sidebar.radio("Go to", list(self.PAGES.keys()))
         page = self.PAGES[selection]
         page.app()
--- src/visualizations/data-journal/pages/page1.py
+++ src/visualizations/data-journal/pages/page1.py
@@ -5,6 +5,9 @@ class Page1:
         self.app()
 
     def app():
-        st.title('Data Extraction')
+        st.title('Estimating Energy Availability')
+        st.title('Task:')
         st.write('Extracting PV Power Potential using Single Diode Formula, KMeans Algorithm, Gaussian Mixture Model, and DBSCAN Algorithm')
-        st.write('These pages would be used to showcase the different notebook files created')
+        st.write('Solar data from 41,933 barangays in the Philippines were processed and analyzed using zonal statistics and QGIS')
+        st.write('From there the data was used to estimate energy availability with the above methods.')
+        st.write('KMeans, Gaussian Mixture, and DBSCAN were then used to get the silhouette score to measure which model to used in identifying potential areas that will be optimal for solar power stations in the Philippines')
