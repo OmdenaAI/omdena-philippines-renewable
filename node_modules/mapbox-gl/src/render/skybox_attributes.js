d9dcc49b52f0b089c8f677e53db29fcfca9a8f99
Bryce Mercines <bryce.mercines@whitecloak.com>
2021-07-31 23:54:21 +0800

    removed out of place node_modules directory

--- node_modules/mapbox-gl/src/render/skybox_attributes.js
+++
@@ -1,9 +0,0 @@
-// @flow
-import {createLayout} from '../util/struct_array.js';
-
-export const skyboxAttributes = createLayout([
-    {name: 'a_pos_3f',  components: 3, type: 'Float32'}
-]);
-
-export default skyboxAttributes;
-export const {members, size, alignment} = skyboxAttributes;
